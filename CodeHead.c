a8dffef9804856d8739f4fc1b74c2f52bdd1f6d3
彩虹海盗 <sudo.free@qq.com>
2021-01-24 15:20:32 +0800

    Create CodeHead.c

---
+++ CodeHead.c
@@ -0,0 +1,7 @@
+/** * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 这个文件来自 GOSCPS(https://github.com/GOSCPS)
+ * @license GOSCPS LICENSE
+ * @file  YourFileNameHere
+ * @brief YourFileContentHere
+ * Copyright (c) 2020 GOSCPS 保留所有权利.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
