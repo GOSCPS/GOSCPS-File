5c8e2ad6a9f933f43c1ca30931f0085b26339e2a
彩虹海盗 <sudo.free@qq.com>
2021-01-31 18:50:49 +0800

    Add Dosygen

--- CodeHead.c
+++ CodeHead.c
@@ -5,3 +5,12 @@
  * Content: YourFileContentHere
  * Copyright (c) 2020-2021 GOSCPS 保留所有权利.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+For Doxygen:
+/** * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 这个文件来自 GOSCPS(https://github.com/GOSCPS)
+ * 使用 GOSCPS 许可证
+ * @author GOSCPS
+ * @file    YourFileContentHere
+ * Copyright (c) 2020-2021 GOSCPS 保留所有权利.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
