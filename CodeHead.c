f39751ebaa9bcb11ac946137801531cd6ce533a5
彩虹海盗 <sudo.free@qq.com>
2021-01-31 18:58:10 +0800

    Update CodeHead.c

--- CodeHead.c
+++ CodeHead.c
@@ -8,9 +8,9 @@
 
 For Doxygen:
 /** * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * 这个文件来自 GOSCPS(https://github.com/GOSCPS)
- * 使用 GOSCPS 许可证
  * @author GOSCPS
- * @file    YourFileContentHere
+ * @license GOSCPS 许可证
+ * @file    YourFileNameHere
+ * @brief   YourFileContentHere
  * Copyright (c) 2020-2021 GOSCPS 保留所有权利.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
