8bd38954d0a4047f97e4574ef8198701b04ac830
彩虹海盗 <sudo.free@qq.com>
2021-02-19 21:59:03 +0800

    Update CodeHead.rs

--- CodeHead.rs
+++ CodeHead.rs
@@ -14,12 +14,3 @@ For Doxygen:
  * @brief   YourFileContentHere \n
  * Copyright (c) 2020-2021 GOSCPS 保留所有权利.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-For Rust
-//! * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-//! * 这个文件来自 GOSCPS(https://github.com/GOSCPS)
-//! * 使用 GOSCPS 许可证
-//! * File:     YourFileNameHere
-//! * Content:  YourFileContentHere
-//! * **Copyright (c) 2020-2021 GOSCPS 保留所有权利.**
-//! * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
