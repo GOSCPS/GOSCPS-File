f789218d5d46d7052647ac4fcba41fc5d285d3bd
彩虹海盗 <sudo.free@qq.com>
2021-05-18 21:55:43 +0800

    Update CodeHead.rs

--- CodeHead.rs
+++ CodeHead.rs
@@ -23,3 +23,10 @@ For Doxygen:
  * @brief   YourFileContentHere \n
  * Copyright (c) 2020-2021 GOSCPS 保留所有权利.
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+For IDEA:
+这个文件来自 GOSCPS(https://github.com/GOSCPS)
+使用 GOSCPS 许可证
+File:    $file.fileName.java
+Content: $file.fileName Source File
+Copyright (c) 2020-$today.year GOSCPS 保留所有权利.
