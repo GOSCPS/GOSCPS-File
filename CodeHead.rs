b791ddaba6628d06529bbba3a60afc7c74e7b8f0
彩虹海盗 <sudo.free@qq.com>
2021-06-05 10:23:36 +0800

    Update CodeHead.rs

--- CodeHead.rs
+++ CodeHead.rs
@@ -30,3 +30,12 @@ For IDEA:
 File:    $file.fileName
 Content: $file.className Source File
 Copyright (c) 2020-$today.year GOSCPS 保留所有权利.
+
+For Visual Studio Templates:
+//=========================================================
+// 这个文件来自 GOSCPS(https://github.com/GOSCPS)
+// 使用 GOSCPS 许可证
+// File:    $safeitemrootname$.cs
+// Content: $safeitemrootname$ Source Code
+// Copyright (c) 2020-2021 GOSCPS 保留所有权利.
+//=========================================================
